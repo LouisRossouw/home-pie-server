c864e29e701c8ae803f217b06ba39bdd3699fb8e
Louis <lourou99@gmail.com>
2025-07-19 11:45:10 +0200

    -

---
+++ projects/time_in_progress/scripts/dev.py
@@ -0,0 +1,19 @@
+import random
+import os
+import sys
+from datetime import datetime
+
+from django.db import connection
+from django.contrib.auth.models import User
+
+
+def run():
+
+    print('Running')
+
+    # set_range = "hours"
+    # set_length = 1
+
+    # account = "time.in.progress"
+
+    # youtube.get_difference(account, set_range, set_length)
