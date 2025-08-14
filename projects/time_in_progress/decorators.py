88436a13d1940a084bd4bb08c04d795263ad7ea2
Louis <lourou99@gmail.com>
2025-08-14 18:01:55 +0200

    Other

--- projects/time_in_progress/decorators.py
+++ projects/time_in_progress/decorators.py
@@ -4,12 +4,24 @@ from functools import wraps
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 
+
 # TODO; Dont allow any
-
-
 def decorator_overview(view_func):
     """Custom decorator to combine multiple DRF decorators."""
     @api_view(['GET'])
+    @permission_classes([AllowAny])
+    @authentication_classes([])
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
+
+
+# TODO; Dont allow any
+def decorator_platform_data(view_func):
+    """Custom decorator to combine multiple DRF decorators."""
+    @api_view(['POST'])
     @permission_classes([AllowAny])
     @authentication_classes([])
     @wraps(view_func)
